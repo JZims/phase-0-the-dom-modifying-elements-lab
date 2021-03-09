723248830e9e54e1a2fd0ce55bf183198b34f0d5
JZims <JZimmsGames@gmail.com>
2021-03-09 00:54:20 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+document.body.querySelector("main").remove();
+
+const newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader);
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Josh is the champion";
+
